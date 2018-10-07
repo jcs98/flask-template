712d17f2ae425f2fe9bc9970843dffb07d930653
Jainam C Shah <jainamcshah98@gmail.com>
2018-10-08 00:09:09 +0530

    Init

---
+++ config.py
@@ -0,0 +1,10 @@
+# Config MySql
+DEBUG = True
+TESTING = True
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'your_username'
+MYSQL_PASSWORD = 'your_password'
+MYSQL_DB = 'myflaskapp'
+MYSQL_CURSORCLASS = 'DictCursor'
+SECRET_KEY='your_secret_key'
+UPLOAD_FOLDER = './uploadFolder'
